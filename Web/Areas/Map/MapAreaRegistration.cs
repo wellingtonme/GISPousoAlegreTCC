6aa56d37d9a6040da488adb1eac8c8385a03480f
Wellington Messias <wellington.mesilva@gmail.com>
2014-08-31 19:36:08 -0300

    Add Map Area

---
+++ Web/Areas/Map/MapAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.Map
+{
+    public class MapAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Map";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Map_default",
+                "Map/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
